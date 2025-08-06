ee3644a396630e44cb4e1fdac612720be977aa49
saidxp <aissoulhcen@gmail.com>
2025-08-06 22:01:07 +0100

    update cordination x,y

--- WALKassetes/main.js
+++ WALKassetes/main.js
@@ -1,72 +1,72 @@
 function creat() {
     const walkingrightFrames = [
-        { x: '-2422px', y: '-15px', width: '76px', height: '207px' },
-        { x: '-2510px', y: '-14px', width: '80px', height: '205px' },
-        { x: '-2602px', y: '-14px', width: '77px', height: '208px' },
-        { x: '-2690px', y: '-15px', width: '80px', height: '203px' }
+        { x: '-457px', y: '-2px', width: '15px', height: '40px' },
+        { x: '-473px', y: '-2px', width: '16px', height: '40px' },
+        { x: '-491px', y: '-2px', width: '15px', height: '40px' },
+        { x: '-507px', y: '-2px', width: '16px', height: '40px' }
     ]
     const walkingrightwithbombs = [ 
-        {x: '-430px', y: '-15px', width:'100px', height : '208px'},
-        {x: '-542px', y :'-14px', width: '100px', height: '204px'},
-        {x: '-654px', y : '-14px', width: '100px', height:'209px'},
-        {x: '-766px', y : '15px', width: '100px', height:'203px'}
+        {x: '-81px', y: '-2px', width:'19px', height : '40px'},
+        {x: '-192px', y :'-2px', width: '20px', height: '40px'},
+        {x: '-123px', y : '-2px', width: '20px', height:'40px'},
+        {x: '-144px', y : '-2px', width: '20px', height:'40px'}
     ];
     
     const walkingleftwithbombs = [
-        {x: '-1227px', y: '-15px', width: '103px', height:'203px'},
-        {x: '-1111px', y: '-15px', width:'104px', height:'207px'},
-        {x: '-995px', y : '-15px', width: '103px', height:'203px'},
-        {x: '-879px', y : '-15px', width:'104px', height:'207px'}
+        {x: '-231px', y: '-2px', width: '20px', height:'40px'},
+        {x: '209-px', y: '-2px', width:'20px', height:'40px'},
+        {x: '-187px', y : '-2px', width: '21px', height:'40px'},
+        {x: '-165px', y : '-2px', width:'21px', height:'40px'}
     ];
     
     const walkingleft = [
-        {x: '-3051px', y:'-15px', width:'80px', height:'203px'},
-        {x: '-2063px', y :'-14px', width: '76px', height:'208px'},
-        {x: '-2871px', y : '-15px', width: '79px', height:'203px'},
-        {x: '-2783px', y : '-14px', width: '75px', height:'208px'}
+        {x: '-575px', y:'-2px', width:'16px', height:'40px'},
+        {x: '-558px', y :'-2px', width: '16px', height:'40px'},
+        {x: '-514px', y : '-2px', width: '16px', height:'40px'},
+        {x: '-524px', y : '-2px', width: '16px', height:'40px'}
     ];
     
     const walkingDown = [
-        {x: '-3143px', y: '-14px', width:'107px', height:'208px'},
-        {x: '-3263px', y : '-14px', width: '104px', height:'212px'},
-        {x: '-3379px', y: '-15px', width:'107px', height:'207px'},
-        {x: '-3498px', y : '-14px', width:'104px', height:'213px'}
+        {x: '-593px', y: '-2px', width:'21px', height:'40px'},
+        {x: '-615px', y : '-2px', width: '21px', height:'41px'},
+        {x: '-637px', y: '-2px', width:'21px', height:'40px'},
+        {x: '-660px', y : '-2px', width:'20px', height:'41px'}
     ];
     
     const walkingDownwithbombs = [
-        {x : '-1343px', y: '-14px', width:'115px', height:'208px'},
-        {x: '-1471px', y: '-14px', width:'112px', height:'212px'},
-        {x: '-1595px', y : '-14px', width:'116px', height:'208px'},
-        {x: '-1723px', y : '-14px', width: '116px', height: '212px'}
-    ];
-    
-    const walkingUp = [
-        {x : '-3615px', y:'-14px', width:'107px', height:'208px'},
-        {x: '-3735px', y:'-14px', width: '104px', height:'212px'},
-        {x: '-3851px', y:'-14px', width:'108px', height:'208px'},
-        {x: '-3971px', y: '-14px', width:'103px', height:'212px'}
+        {x : '-253px', y: '-2px', width:'23px', height:'40px'},
+        {x: '-277px', y: '-2px', width:'22px', height:'41px'},
+        {x: '-300px', y : '-2px', width:'23px', height:'40px'},
+        {x: '-325px', y : '-2px', width: '22px', height: '41px'}
     ];
     
     const walkingUpwithbombs = [
-        {x: '-1851px', y : '-14px', width: '131px', height:'208px'},
-        {x: '-1995px', y: '-14px', width: '131px', height:'212px'},
-        {x: '-2139px', y :'-15px', width: '131px', height:'207px'},
-        {x: '-2283px', y: '-15px', width:'127px', height:'211px'}
+        {x : '-349px', y:'-2px', width:'25px', height:'40px'},
+        {x: '-376px', y:'-2px', width: '26px', height:'41px'},
+        {x: '-403px', y:'-2px', width:'26px', height:'40px'},
+        {x: '-430px', y: '-2px', width:'25px', height:'41px'}
+    ];
+    
+    const walkingUp = [
+        {x: '-682px', y : '-2px', width: '21px', height:'40px'},
+        {x: '-704px', y: '-2px', width: '21px', height:'41px'},
+        {x: '-726px', y :'-2px', width: '21px', height:'40px'},
+        {x: '-749px', y: '-2px', width:'20px', height:'41px'}
     ];
     
     const right = [
         // wa9f taychoof 3la limn
-        {x: '-15px', y: '-14px' , width: '75px', height:'209px'}
+        {x: '-2px', y: '-2px' , width: '15px', height:'40px'}
     ]
     const rightwithbombs = [
-        {x: '-15px', y :'208px', width: '99px', height: '208px'}
+        {x: '-2px', y :'-45px', width: '20px', height: '40px'}
     ]
     const left = [
         // wa9f taychof 3la lissar 
-        {x: '-103px', y:'-15px', width: '75px', height: '208px'}
+        {x: '-18px', y:'-2px', width: '16px', height: '40px'}
     ]
     const leftwithbomb = [
-        {x: '-127px', y:'-239px', width:'103px', height:'207px'}
+        {x: '-23px', y:'-45px', width:'21px', height:'40px'}
     ]
     // chtiha not detect 
 
