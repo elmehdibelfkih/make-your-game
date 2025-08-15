44541847b8f66abac14ff99f561e283d2973142d
soubaaiss <aissoulhcen@gmail.com>
2025-08-16 00:13:41 +0100

    not work

--- components/enemy.js
+++ components/enemy.js
@@ -6,7 +6,7 @@ export class Enemy {
         //this.y = y
         //this.xMap = 11
         //this.yMap = 11
-        this.enemies = []; 
+        this.enemies = [];
         this.level = level
         this.createnemy();
 
@@ -14,7 +14,7 @@ export class Enemy {
         //console.log(this.enemies); 
         //this.check();
     }
-     
+
     //console.log('Enemy start coords:', game.map.level.enemy_x, game.map.level.enemy_y);
     // i Will remove this instance  later !!
     static getInstance(game, initialLevel) {
@@ -25,7 +25,7 @@ export class Enemy {
         return Enemy.instance
     }
     // ==> I Want To CreatE Enemy <==!! \\
-     
+
     // Debugging: Log the enemies array
     //console.log(this.enemies);
     //console.log(this.enemies)
@@ -42,24 +42,25 @@ export class Enemy {
             });
         });
     }
-    update(){
+    update() {
         this.enemies.map((el) => el.Canmoveandupdate())
     }
-    
+
 }
 
 // Update logic of the enemies >.
 
-export class Emy{
+export class Emy {
 
-    constructor(game ,level, x ,y) {
+    constructor(game, level, x, y) {
         this.game = game
-        this.x = x 
+        this.x = x
         this.y = y
         this.level = level
+       // this.direction = "left"
         this.creatediv()
     }
-    
+
     creatediv() {
         this.domElement = document.createElement('div');
         this.domElement.className = 'enemy';
@@ -72,25 +73,46 @@ export class Emy{
         this.game.map.grid.appendChild(this.domElement);
         this.domElement.style.transform = `translate(${this.x}px, ${this.y}px)`;
     }
+    canMove(dx, dy) {
+        const blockSize = this.level.block_size;
+        const map = this.level.map;
+
+         const enemySize = blockSize;
+
+        // Check All Four Corners After The Move <==> !!
+        const corners = [
+            { x: this.x + dx, y: this.y + dy }, // top-left
+            { x: this.x + dx + enemySize - 1, y: this.y + dy }, // top-right
+            { x: this.x + dx, y: this.y + dy + enemySize - 1 }, // bottom-left
+            { x: this.x + dx + enemySize - 1, y: this.y + dy + enemySize - 1 }  // bottom-right
+        ];
+
+        return corners.every(corner => {
+            const col = Math.floor(corner.x / blockSize);
+            const row = Math.floor(corner.y / blockSize);
+            return map[row] && map[row][col] === 0;  
+        });
+    }
 
     Canmoveandupdate() {
-            const blockSize = this.level.block_size;
-            const map = this.level.map;
-            const col = Math.floor(this.x / blockSize);
-            const row = Math.floor(this.y / blockSize);
+        let dx = 0, dy = 0;
+        switch (this.direction) {
+            case 'right': dx = 1; break;
+            case 'left': dx = -1; break;
+            case 'down': dy = 1; break;
+            case 'up': dy = -1; break;
+        }
 
-            const maxRow = map.length - 1;
-            const maxCol = map[0].length - 1;
+        if (this.canMove(dx, dy)) {
+            this.x += dx;
+            this.y += dy;
+        } else {
+            const directions = ['right', 'left', 'down', 'up'];
+            this.direction = directions[Math.floor(Math.random() * directions.length)];
+        }
 
-            const canMoveRight = col < maxCol && map[row][col + 1] === 0;
-            const canMoveLeft  = col > 0 && map[row][col - 1] === 0;
-            const canMoveDown  = row < maxRow && map[row + 1][col] === 0;
-            const canMoveUp    = row > 0 && map[row - 1][col] === 0;
-
-            if (canMoveRight) this.x += 1;
-            else if (canMoveLeft) this.x -= 1;
-            else if (canMoveDown) this.y += 1;
-            else if (canMoveUp) this.y -= 1;
-            this.domElement.style.transform = `translate(${this.x + 12}px, ${this.y + 11}px)`;
+        this.domElement.style.transform = `translate(${this.x + 12}px, ${this.y + 11}px)`;
     }
+
+
 }
